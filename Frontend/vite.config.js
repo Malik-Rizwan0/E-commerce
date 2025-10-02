b703bac5b69ccd2e7dbbb9a64016d39b042df5ae
Malik-Rizwan0 <malikrizwan107@gmail.com>
2025-10-02 16:58:24 +0500

    modify vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://e-commerce-ji7u.onrender.com/',
         changeOrigin: true,
         secure: false,
       },
